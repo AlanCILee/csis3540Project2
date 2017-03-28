a33445e6514cd4d8a636291ad566fad21c502e66
AlanCILee <alan.changil.lee@gmail.com>
2017-03-28 14:12:31 -0700

    basic stock display

---
+++ ElectronicMart/SellerForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ElectronicMart
+{
+    partial class SellerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbStock = new System.Windows.Forms.Label();
+            this.gridViewStock = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbStock
+            // 
+            this.lbStock.AutoSize = true;
+            this.lbStock.Location = new System.Drawing.Point(13, 31);
+            this.lbStock.Name = "lbStock";
+            this.lbStock.Size = new System.Drawing.Size(72, 13);
+            this.lbStock.TabIndex = 0;
+            this.lbStock.Text = "Stock Search";
+            // 
+            // gridViewStock
+            // 
+            this.gridViewStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewStock.Location = new System.Drawing.Point(16, 47);
+            this.gridViewStock.Name = "gridViewStock";
+            this.gridViewStock.Size = new System.Drawing.Size(667, 264);
+            this.gridViewStock.TabIndex = 1;
+            // 
+            // SellerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1240, 693);
+            this.Controls.Add(this.gridViewStock);
+            this.Controls.Add(this.lbStock);
+            this.Name = "SellerForm";
+            this.Text = "Seller Application";
+            this.Load += new System.EventHandler(this.SellerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewStock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbStock;
+        private System.Windows.Forms.DataGridView gridViewStock;
+    }
+}
+
---
+++ ElectronicMart/SellerForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAdministrator.EF_Classes;
+using System.Data.Entity; 
+
+namespace ElectronicMart
+{ 
+    public partial class SellerForm : Form
+    {
+        private ElectronicsMartEntities context;
+
+        public SellerForm()
+        {
+            InitializeComponent();
+            context = new ElectronicsMartEntities();
+        }
+
+        
+
+        private void SellerForm_Load(object sender, EventArgs e)
+        {
+            context.Products.Load();
+            context.Categories.Load();
+
+            var query = from product in context.Products
+                        join category in context.Categories
+                        on product.categoryId equals category.categoryId
+                        select new
+                        {
+                            ProductID = product.productId,
+                            Category = category.categoryName,
+                            ProductName = product.productName,
+                            Price = product.unitPrice,
+                            QTY = product.quantityAvailable,
+                            Cost = product.primeCost
+                        };
+
+
+            gridViewStock.DataSource = query.ToList();
+            gridViewStock.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            //gridViewStock.Columns[2].Width = 10;
+            //gridViewStock.Columns[3].Width = 40;
+            //gridViewStock.Columns[4].Width = 40;
+            //gridViewStock.Columns[5].Width = 40;
+        }
+    }
+}
