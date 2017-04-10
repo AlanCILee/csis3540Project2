5547a7c0c9f055c80b1483288de9a0bd3ee6ddbc
DESKTOP-RGE5KT3\Python <sidharth.kaushal64@gmail.com>
2017-04-10 13:37:31 -0700

    Products to cart

---
+++ BuyerApplication/BuyerForm.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace BuyerApplication
+{
+    partial class BuyerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.buttonAddToCart = new System.Windows.Forms.Button();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridViewCart = new System.Windows.Forms.DataGridView();
+            this.buttonOrder = new System.Windows.Forms.Button();
+            this.buttonClearCart = new System.Windows.Forms.Button();
+            this.labelOrderTotal = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBoxMenu = new System.Windows.Forms.GroupBox();
+            this.dataGridViewProducts = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCart)).BeginInit();
+            this.groupBoxMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(461, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The Electronics Mart";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Category:";
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Location = new System.Drawing.Point(133, 36);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(211, 23);
+            this.comboBoxCategory.TabIndex = 3;
+            this.comboBoxCategory.SelectedIndexChanged += new System.EventHandler(this.comboBoxCategory_SelectedIndexChanged);
+            // 
+            // buttonAddToCart
+            // 
+            this.buttonAddToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddToCart.Location = new System.Drawing.Point(352, 453);
+            this.buttonAddToCart.Name = "buttonAddToCart";
+            this.buttonAddToCart.Size = new System.Drawing.Size(153, 38);
+            this.buttonAddToCart.TabIndex = 5;
+            this.buttonAddToCart.Text = "Add To Cart";
+            this.buttonAddToCart.UseVisualStyleBackColor = true;
+            this.buttonAddToCart.Click += new System.EventHandler(this.buttonAddToCart_Click);
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(184, 460);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(94, 22);
+            this.textBoxQuantity.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 463);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter Quantity:";
+            // 
+            // dataGridViewCart
+            // 
+            this.dataGridViewCart.AllowUserToAddRows = false;
+            this.dataGridViewCart.AllowUserToDeleteRows = false;
+            this.dataGridViewCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCart.Location = new System.Drawing.Point(653, 104);
+            this.dataGridViewCart.Name = "dataGridViewCart";
+            this.dataGridViewCart.ReadOnly = true;
+            this.dataGridViewCart.Size = new System.Drawing.Size(351, 184);
+            this.dataGridViewCart.TabIndex = 8;
+            // 
+            // buttonOrder
+            // 
+            this.buttonOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOrder.Location = new System.Drawing.Point(98, 295);
+            this.buttonOrder.Name = "buttonOrder";
+            this.buttonOrder.Size = new System.Drawing.Size(162, 42);
+            this.buttonOrder.TabIndex = 10;
+            this.buttonOrder.Text = "Order";
+            this.buttonOrder.UseVisualStyleBackColor = true;
+            // 
+            // buttonClearCart
+            // 
+            this.buttonClearCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClearCart.Location = new System.Drawing.Point(116, 343);
+            this.buttonClearCart.Name = "buttonClearCart";
+            this.buttonClearCart.Size = new System.Drawing.Size(129, 42);
+            this.buttonClearCart.TabIndex = 11;
+            this.buttonClearCart.Text = "Clear Cart";
+            this.buttonClearCart.UseVisualStyleBackColor = true;
+            // 
+            // labelOrderTotal
+            // 
+            this.labelOrderTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelOrderTotal.Location = new System.Drawing.Point(178, 254);
+            this.labelOrderTotal.Name = "labelOrderTotal";
+            this.labelOrderTotal.Size = new System.Drawing.Size(150, 23);
+            this.labelOrderTotal.TabIndex = 12;
+            this.labelOrderTotal.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Order Total:";
+            // 
+            // groupBoxMenu
+            // 
+            this.groupBoxMenu.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBoxMenu.Controls.Add(this.dataGridViewProducts);
+            this.groupBoxMenu.Controls.Add(this.label2);
+            this.groupBoxMenu.Controls.Add(this.comboBoxCategory);
+            this.groupBoxMenu.Controls.Add(this.buttonAddToCart);
+            this.groupBoxMenu.Controls.Add(this.textBoxQuantity);
+            this.groupBoxMenu.Controls.Add(this.label3);
+            this.groupBoxMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBoxMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxMenu.Location = new System.Drawing.Point(25, 65);
+            this.groupBoxMenu.Name = "groupBoxMenu";
+            this.groupBoxMenu.Size = new System.Drawing.Size(583, 522);
+            this.groupBoxMenu.TabIndex = 14;
+            this.groupBoxMenu.TabStop = false;
+            this.groupBoxMenu.Text = "Menu";
+            // 
+            // dataGridViewProducts
+            // 
+            this.dataGridViewProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProducts.Location = new System.Drawing.Point(28, 87);
+            this.dataGridViewProducts.Name = "dataGridViewProducts";
+            this.dataGridViewProducts.Size = new System.Drawing.Size(530, 346);
+            this.dataGridViewProducts.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.buttonClearCart);
+            this.groupBox1.Controls.Add(this.labelOrderTotal);
+            this.groupBox1.Controls.Add(this.buttonOrder);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(643, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 410);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cart";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(138, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "My Cart";
+            // 
+            // BuyerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 606);
+            this.Controls.Add(this.dataGridViewCart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBoxMenu);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BuyerForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCart)).EndInit();
+            this.groupBoxMenu.ResumeLayout(false);
+            this.groupBoxMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxCategory;
+        private System.Windows.Forms.Button buttonAddToCart;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridViewCart;
+        private System.Windows.Forms.Button buttonOrder;
+        private System.Windows.Forms.Button buttonClearCart;
+        private System.Windows.Forms.Label labelOrderTotal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBoxMenu;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridViewProducts;
+    }
+}
+
---
+++ BuyerApplication/BuyerForm.cs
@@ -0,0 +1,121 @@
+﻿using DataAdministrator.EF_Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuyerApplication
+{
+    public partial class BuyerForm : Form
+    {
+        private ElectronicsMartEntities context;
+        List<Product> productsToAdd = new List<Product>();
+
+        public BuyerForm()
+        {
+            InitializeComponent();
+
+            context = new ElectronicsMartEntities();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            context.Categories.Load();
+            context.Customers.Load();
+            context.Orders.Load();
+            context.Products.Load();
+
+            comboBoxCategory.DataSource = context.Categories.ToList();
+            comboBoxCategory.ValueMember = "categoryId";
+            comboBoxCategory.DisplayMember = "categoryName";
+            comboBoxCategory.Invalidate();
+
+        }
+
+        private void comboBoxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           int selectedCategory =comboBoxCategory.SelectedIndex;
+
+
+            var query = from category in context.Products
+                        where category.categoryId == selectedCategory || selectedCategory==0 
+                        select new
+                        {
+                            ProductID = category.productId,
+                            Category = category.Category.categoryName,
+                            ProductName = category.productName,
+                            Price = category.unitPrice,
+                            QTY = category.quantityAvailable
+                        };
+            dataGridViewProducts.DataSource = query.ToList();
+            dataGridViewProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+           
+            
+        }
+
+        private void buttonAddToCart_Click(object sender, EventArgs e)
+        {
+            int quantity;
+
+            if (textBoxQuantity.Text == "")
+            {
+                MessageBox.Show("Please enter quantity");
+                return;
+            }
+            else
+                quantity = int.Parse(textBoxQuantity.Text);
+
+            
+
+            if (dataGridViewProducts.SelectedRows.Count == 0)
+                return;
+
+
+            
+
+
+            foreach (DataGridViewRow row in dataGridViewProducts.SelectedRows)
+               productsToAdd.Add(context.Products.Find(row.Cells[0].Value));
+
+           
+               var query = from orders in productsToAdd
+                            select new
+                            {
+                                ProductId = productsToAdd.ElementAt(0).productId,
+                                UnitPrice = productsToAdd.ElementAt(0).unitPrice,
+                                Quantity = quantity,
+                                TotalPrice = (quantity * (productsToAdd.ElementAt(0).unitPrice))
+
+                            };
+            
+            dataGridViewCart.DataSource = query.ToList();
+            dataGridViewCart.Refresh();
+            dataGridViewCart.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            
+
+
+            
+           // updateCart();
+
+
+
+
+
+
+        }
+
+        private void updateCart()
+        {
+            throw new NotImplementedException();
+        }
+
+    
+    }
+}
